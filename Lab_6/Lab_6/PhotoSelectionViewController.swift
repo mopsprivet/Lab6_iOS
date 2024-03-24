e0d2bf0a6146f38ccfcadc74548f9f32c4646e54
mopsprivet <115881344+mopsprivet@users.noreply.github.com>
2024-03-24 23:52:48 +0500

    Add files via upload

---
+++ Lab_6/Lab_6/PhotoSelectionViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class PhotoSelectionViewController: UIViewController {
+
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
